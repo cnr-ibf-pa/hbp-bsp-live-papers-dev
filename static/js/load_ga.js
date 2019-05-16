c0900c4ca2bed02a4656eae842eb3cfb0031a7b4
Luca Leonardo Bologna <lucaleonardo.bologna@pa.ibf.cnr.it>
2019-05-16 19:17:45 +0200

    modified code for .src setting

--- static/js/load_ga.js
+++ static/js/load_ga.js
@@ -1,15 +1,16 @@
 var head = document.getElementsByTagName('head')[0];
-var script = document.createElement('script');
-script.type = 'text/javascript';
+var ga_script = document.createElement('SCRIPT');
+ga_script.type = 'text/javascript';
 
 var url = window.location.href;
 
 if (url.includes("https://humanbrainproject.github.io/hbp-bsp-live-papers/") ||
     url.includes("https://collab.humanbrainproject.eu")){
-    script.src('epfl_bspg_analytics.js');
+    ga_script.src = 'epfl_bspg_analytics.js';
 } else if (url.includes("https://cnr-ibf-pa.github.io/hbp-bsp-live-papers-dev/")){
-    script.src('ibf_bspg_analytics.js')
+    ga_script.src = 'ibf_bspg_analytics.js';
 } else {
     console.log("Loading locally or from an unknown domain");
 } 
 
+head.prepend(ga_script);
