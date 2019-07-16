eaf332d2ab3945ef104a8b835a65ffb5489a8a86
Luca Leonardo Bologna <lucaleonardo.bologna@pa.ibf.cnr.it>
2019-07-16 18:49:42 +0200

    changed analytics file names

--- static/js/load_ga.js
+++ static/js/load_ga.js
@@ -10,11 +10,11 @@ if (url.includes("https://humanbrainproject.github.io/hbp-bsp-live-papers/") ||
     url.includes("https://collab.humanbrainproject.eu")){
 	console.log("Loading ga for epfl")
     ga_script_g.src = 'https://www.googletagmanager.com/gtag/js?id=UA-91794319-5';
-    ga_script.src = 'https://humanbrainproject.github.io/hbp-bsp-live-papers-dev/static/js/epfl_bspg_analytics.js';
+    ga_script.src = 'https://humanbrainproject.github.io/hbp-bsp-live-papers-dev/static/js/lp_prod_analytics.js';
 } else if (url.includes("https://cnr-ibf-pa.github.io/hbp-bsp-live-papers-dev/")){
 	console.log("Loading ga for bspg")
     ga_script_g.src = 'https://www.googletagmanager.com/gtag/js?id=UA-91794319-4';
-    ga_script.src = 'https://cnr-ibf-pa.github.io/hbp-bsp-live-papers-dev/static/js/ibf_bspg_analytics.js';
+    ga_script.src = 'https://cnr-ibf-pa.github.io/hbp-bsp-live-papers-dev/static/js/lp_dev_analytics.js';
 } else {
     console.log("Loading locally or from an unknown domain");
 } 
